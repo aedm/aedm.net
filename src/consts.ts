f2dd3e4091486a04cda0cb9db7ba18185822e9b6
Gábor Gyebnár <korteur@gmail.com>
2025-12-12 00:27:03 +0100

    Less fluff

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'aedm.net';
 export const SITE_DESCRIPTION = 'Personal blog about software engineering, Rust, graphics programming, and other tech adventures.';
-export const AUTHOR_NAME = 'aedm';
